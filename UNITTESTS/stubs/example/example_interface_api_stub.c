0629d7ee3efedfdfcbeb4c9f47fe64f870d3149b
bmatsuda <matsudbc@gmail.com>
2023-11-05 20:52:55 +0000

    I hate Testing

---
+++ UNITTESTS/stubs/example/example_interface_api_stub.c
@@ -0,0 +1,11 @@
+#include "example_interface/example_interface_api.h"
+
+example_interface_status_t example_interface_api_init()
+{
+    return STATUS_OK;
+}
+
+void example_interface_api_start(void)
+{
+
+}
